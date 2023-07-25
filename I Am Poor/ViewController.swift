df1af943261b688853e1cb96b0eb24fc3ef71173
Danila Bolshakov <danila1999777@gmail.com>
2023-07-25 13:40:42 +0300

    Initial commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Danila Bolshakov on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
